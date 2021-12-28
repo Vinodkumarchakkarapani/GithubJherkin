527762d7035d52fc4a33a0902c3e83c9dcca0fa2
Vinodkumarchakkarapani <vinowenow@gmail.com>
2021-12-28 11:45:16 +0530

    initial

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,8 +2,9 @@ class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("Hello, Vinod! How are You?");
-         System.out.println("I'm Fine");
+        System.out.println("I'm Fine");
         System.out.println("How about you?");
         System.out.println("Yeah! I'm good");
+        System.out.println("Yeah! I'm okay");
     }
 }
